d9bde427b98a1e220e5b886e79ec96c4ef04ede3
Maulanahb <maulanahmad2287@gmail.com>
2025-10-06 22:31:04 +0700

    menambahkan preg replace dan match

--- regex.php
+++ regex.php
@@ -6,4 +6,27 @@ if (preg_match($pattern, $text)) {
 }else{
     echo "Tidak ada huruf kecil.";
 }
+echo"<br>";
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+}else{
+    echo "Tidak ada angka.";
+}
+echo "<br>";
+$pattern = "/apple/";
+$replacement ='banana';
+$text='I like apple pie';
+$new_text = preg_replace($pattern, $replacement,$text);
+echo $new_text;//output: i like banana pie
+
+echo '<br>';
+$pattern = 'go?d';
+$text ='good god gd';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+}else{
+    echo "Tidak ada angka.";
+}
 ?>
